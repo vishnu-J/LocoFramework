24bdbb6ffc7cf9b4622260a4e24348d3474fb9d2
Vishnu <vishnu@greedygame.com>
2018-09-07 12:17:46 +0530

    Initial Commit

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by vishnu j on 07/09/18.
+//  Copyright © 2018 Vishnu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by vishnu j on 07/09/18.
+//  Copyright © 2018 Vishnu. All rights reserved.
+//
+
+import UIKit
+
+public class Service {
+    
+    private init() {
+        
+    }
+
+    public func sampleService() -> String {
+        return "The string from framework"
+    }
+}
